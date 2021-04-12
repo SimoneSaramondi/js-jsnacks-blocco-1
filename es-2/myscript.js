32022771f6e2adc69a30ce46e68156c267a1f502
SimoneSaramondi <simo98sara@gmail.com>
2021-04-12 12:08:11 +0200

    final second project

--- es-2/myscript.js
+++ es-2/myscript.js
@@ -1,9 +1,15 @@
 var somma = 0;
 var numero;
 
-for (var i = 0; i < 10; i++) {    
-    if (!Number.isNaN(numero)){
+for (var i = 0; i < 10; i++) {
+    numero = parseInt(prompt("Inserisci il " + i + " ^ numero"));
+    
+    if (!Number.isNaN(numero)) {
+        somma += numero;
+    }else{
+        numero = parseInt(prompt("Non hai inserito un numero corretto Reinserisci il " + i + " ^ numero"));
         somma += numero;
     }
 }
 console.log("La somma dei dieci numeri è: " + somma);
+document.getElementById("somma").innerHTML="La somma della serie è: " + somma;
